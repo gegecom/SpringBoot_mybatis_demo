e54f840c6673a5bd32450fb9a2d3ad5894ad32e5
geshiying <geshiying@163.com>
2018-10-25 11:39:41 +0800

    逆向生成mapper和映射的实体类

---
+++ src/main/java/com/example/demo/mapper/TUserMapper.java
@@ -0,0 +1,7 @@
+package com.example.demo.mapper;
+
+import com.example.demo.pojo.TUser;
+import com.example.demo.utils.MyMapper;
+
+public interface TUserMapper extends MyMapper<TUser> {
+}
---
+++ src/main/java/com/example/demo/pojo/TUser.java
@@ -0,0 +1,73 @@
+package com.example.demo.pojo;
+
+import javax.persistence.*;
+
+@Table(name = "t_user")
+public class TUser {
+    @Id
+    @Column(name = "UserId")
+    private Integer userid;
+
+    @Column(name = "userName")
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    /**
+     * @return UserId
+     */
+    public Integer getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid
+     */
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return userName
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    /**
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    /**
+     * @return phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone
+     */
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
