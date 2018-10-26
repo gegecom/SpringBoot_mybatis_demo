73fd362d73c6e8fa59fdd75b701bdd7c05375a51
geshiying <geshiying@163.com>
2018-10-26 17:43:02 +0800

    更改为数据库表的结构文件

--- src/main/resources/sql/t_user.sql
+++ src/main/resources/sql/t_user.sql
@@ -1,2 +1,35 @@
-INSERT INTO `t_user` (`UserId`, `userName`, `password`, `phone`) VALUES (1000, 'gsy', '123456', '123456');
-INSERT INTO `t_user` (`UserId`, `userName`, `password`, `phone`) VALUES (1001, 'gsy2', '123456', '123123');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhostMysql
+Source Server Version : 80012
+Source Host           : localhost:3306
+Source Database       : dbgril
+
+Target Server Type    : MYSQL
+Target Server Version : 80012
+File Encoding         : 65001
+
+Date: 2018-10-26 17:41:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1000', 'gsy', '123456', '123456');
+INSERT INTO `t_user` VALUES ('1001', 'gsy2', '123456', '123123');
+INSERT INTO `t_user` VALUES ('1002', 'gesy', '123456', '12345678900');
--- src/main/resources/sql/userinfo.sql
+++ src/main/resources/sql/userinfo.sql
@@ -1,2 +1,32 @@
-INSERT INTO `userinfo` (`name`, `id`) VALUES ('gsy', 1);
-INSERT INTO `userinfo` (`name`, `id`) VALUES ('gsy', 2);
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhostMysql
+Source Server Version : 80012
+Source Host           : localhost:3306
+Source Database       : dbgril
+
+Target Server Type    : MYSQL
+Target Server Version : 80012
+File Encoding         : 65001
+
+Date: 2018-10-26 17:42:08
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for userinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `userinfo`;
+CREATE TABLE `userinfo` (
+  `name` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of userinfo
+-- ----------------------------
+INSERT INTO `userinfo` VALUES ('gsy', '1');
+INSERT INTO `userinfo` VALUES ('gsy', '2');
